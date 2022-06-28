4f58205c8e20555cf7c4b573da237d610f3cfbaa
Molly <molly.jones@launchdarkly.com>
2022-06-28 16:22:07 -0700

    add standard comments and outputs

--- index.ts
+++ index.ts
@@ -6,8 +6,6 @@ const sdkKey = "";
 // Set featureFlagKey to the feature flag key you want to evaluate.
 const featureFlagKey = "my-boolean-flag";
 
-const client = LaunchDarkly.init(sdkKey);
-
 // Set up the user properties. This use should appear on your LaunchDarkly
 // users dashboard soon after you run the demo.
 const user = {
@@ -15,17 +13,30 @@ const user = {
    "key": "example-user-key"
 };
 
+function showMessage(s: string) {
+  console.log("*** " + s);
+  console.log("");
+}
+
+const client = LaunchDarkly.init(sdkKey);
+
 client.once('ready', function() {
-  // TODO : Enter the key for your feature flag here
-  client.variation("enable-pinning", user, false, function(err, showFeature) {
+  showMessage("SDK successfully initialized!");
+  client.variation(featureFlagKey, user, false, function(err, showFeature) {
     client.track("event-called", user);
     if (showFeature) {
       // application code to show the feature
-      console.log("Showing your feature to " + user.key );
+      showMessage("Feature flag '" + featureFlagKey + "' is true for this user");
     } else {
-      // the code to run if the feature is off 
-      console.log("Not showing your feature to " + user.key);
+      // the code to run if the feature is off
+      showMessage("Feature flag '" + featureFlagKey + "' is false for this user");
     }
+
+    // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+    // events to LaunchDarkly before the program exits. If analytics events are not delivered,
+    // the user properties and flag usage statistics will not appear on your dashboard. In a
+    // normal long-running application, the SDK would continue running and events would be
+    // delivered automatically in the background.
     client.flush(function() {
       client.close();
     });
