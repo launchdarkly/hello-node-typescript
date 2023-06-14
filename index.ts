2283ed9e1f44e5fac4708cc8eada27cf46bc3993
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-06-14 13:48:14 -0700

    Update for node SDK 8.0.0 release.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as LaunchDarkly from 'launchdarkly-node-server-sdk';
+import * as LaunchDarkly from '@launchdarkly/node-server-sdk';
 
 // Set sdkKey to your LaunchDarkly SDK key.
 const sdkKey = "";
@@ -6,11 +6,12 @@ const sdkKey = "";
 // Set featureFlagKey to the feature flag key you want to evaluate.
 const featureFlagKey = "my-boolean-flag";
 
-// Set up the user properties. This use should appear on your LaunchDarkly
-// users dashboard soon after you run the demo.
-const user = {
-   "name": "Sandy",
-   "key": "example-user-key"
+// Set up the context properties. This use should appear on your LaunchDarkly
+// contexts dashboard soon after you run the demo.
+const context = {
+  "kind": "user",
+  "name": "Sandy",
+  "key": "example-context-key"
 };
 
 function showMessage(s: string) {
@@ -20,24 +21,24 @@ function showMessage(s: string) {
 
 const client = LaunchDarkly.init(sdkKey);
 
-client.once('ready', function() {
+client.once('ready', function () {
   showMessage("SDK successfully initialized!");
-  client.variation(featureFlagKey, user, false, function(err, showFeature) {
-    client.track("event-called", user);
+  client.variation(featureFlagKey, context, false, function (err, showFeature) {
+    client.track("event-called", context);
     if (showFeature) {
       // application code to show the feature
-      showMessage("Feature flag '" + featureFlagKey + "' is true for this user");
+      showMessage("Feature flag '" + featureFlagKey + "' is true for this context");
     } else {
       // the code to run if the feature is off
-      showMessage("Feature flag '" + featureFlagKey + "' is false for this user");
+      showMessage("Feature flag '" + featureFlagKey + "' is false for this context");
     }
 
     // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
     // events to LaunchDarkly before the program exits. If analytics events are not delivered,
-    // the user properties and flag usage statistics will not appear on your dashboard. In a
+    // the context properties and flag usage statistics will not appear on your dashboard. In a
     // normal long-running application, the SDK would continue running and events would be
     // delivered automatically in the background.
-    client.flush(function() {
+    client.flush(function () {
       client.close();
     });
   });
